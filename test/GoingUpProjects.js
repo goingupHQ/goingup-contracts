cd51de7a401bfb3f5e0b948033d48283896bbcda
Mark Ibanez <mark.ibanez@gmail.com>
2022-06-05 22:52:00 +0800

    Update GoingUpProjects.js

--- test/GoingUpProjects.js
+++ test/GoingUpProjects.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-let contract, contractAsDeployer, contractAsOwner, contractAsAdmin1, contractAsAdmin2;
-let deployer, owner, admin1, admin2;
+let contract, contractAsDeployer, contractAsOwner, contractAsAdmin1, contractAsAdmin2, contractAsPublic1;
+let deployer, owner, admin1, admin2, public1;
 
 describe('Intialize', () => {
     it('Deploy GoingUpProjects Contract', async () => {
@@ -11,13 +11,15 @@ describe('Intialize', () => {
         owner = await signers[1].getAddress();
         admin1 = await signers[2].getAddress();
         admin2 = await signers[3].getAddress();
+        public1 = await signers[4].getAddress();
 
         const GoingUpProjects = await ethers.getContractFactory('GoingUpProjects', signers[0]);
         contract = await GoingUpProjects.deploy();
         await contract.deployed();
 
-        [contractAsDeployer, contractAsOwner, contractAsAdmin1, contractAsAdmin2] =
-            (await ethers.getSigners()).map(signer => contract.connect(signer));
+        [contractAsDeployer, contractAsOwner, contractAsAdmin1, contractAsAdmin2, contractAsPublic1] = (
+            await ethers.getSigners()
+        ).map((signer) => contract.connect(signer));
     });
 });
 
@@ -48,5 +50,36 @@ describe('Contract admins', () => {
         await expect(contractAsAdmin1.setAdmin(admin1, false)).to.be.revertedWith('not the owner');
         await expect(contractAsAdmin2.setAdmin(admin2, true)).to.be.revertedWith('not the owner');
         await expect(contractAsAdmin2.setAdmin(admin2, false)).to.be.revertedWith('not the owner');
+    });
+
+    it('Owner sets admin addresses as admin', async () => {
+        await contractAsOwner.setAdmin(admin1, true);
+        await contractAsOwner.setAdmin(admin2, true);
+        expect(await contract.admins(admin1)).to.equal(true);
+        expect(await contract.admins(admin2)).to.equal(true);
+    });
+
+    it('Owner unsets and resets admin2 address as admin', async () => {
+        await contractAsOwner.setAdmin(admin2, false);
+        expect(await contract.admins(admin2)).to.equal(false);
+        await contractAsOwner.setAdmin(admin2, true);
+        expect(await contract.admins(admin2)).to.equal(true);
+    });
+});
+
+describe('Contract variable "price"', () => {
+    it('Variable "price" getter', async () => {
+        expect(await contractAsPublic1.price()).to.equal('10000000000000000');
+    });
+
+    it('Non admin address tries to set price', async () => {
+        await expect(contractAsPublic1.setPrice('25000000000000000')).to.be.revertedWith('not admin');
+    })
+
+    it('Admin address sets price', async () => {
+        await contractAsAdmin1.setPrice('25000000000000000');
+        expect(await contractAsPublic1.price()).to.equal('25000000000000000');
+        await contractAsAdmin2.setPrice('35000000000000000');
+        expect(await contractAsPublic1.price()).to.equal('35000000000000000');
     })
 });
